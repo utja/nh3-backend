7dd624dcf96abd216fa408ecdedaf66258ddb34a
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-29 23:47:51 -0400

    add category resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :categories
+      # resources :users, only: %i[create update]
+      # post '/login', to: 'auth#create'
+      # get '/profile', to: 'users#profile'
+    end
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
